3da087bc1672861c3227d9d4c634f9826766cb71
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-08-28 15:27:21 +0300

    Tehtävä 1.5 toimii, mut 'key' error

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -10,29 +10,19 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
-    </div>
+  return props.parts.map((value) =>
+    <Part part={value} />
   )
 }
 
 const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
+  return (<p>{props.part['name'] + ' ' + props.part['exercises']}</p>)
 }
 
 const Total = (props) => {
   return(
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}</p>
     </div>
   )
 }
@@ -55,30 +45,12 @@ const App = () => {
       exercises: 14
     }
 ]
-  
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  
-  console.log('Debuggaus: ')
-  console.log(parts[2])
-  console.log(parts[2].['name'])
-  console.log(parts[2].['exercises'])
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
