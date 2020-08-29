381d6488ed99222f81394dec0fb95057dff0cf2d
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-08-29 19:48:54 +0300

    Step 2 kesken

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Results = ({ good, neutral, bad }) =>{
+const Statistics = ({ good, neutral, bad, average, feedback }) =>{
   return (
     <div>
       <p>Statistics</p>
-      <p>Good {good}<br />Neutral {neutral}<br />Bad {bad}</p>
+      <p>
+        Good {good}<br />
+        Neutral {neutral}<br />
+        Bad {bad}<br />
+        Average {average}<br />
+        Feedback amount {feedback} 
+      </p>
     </div>
   )
 }
@@ -17,10 +23,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [feedback, setFeedback] = useState(0)
 
-  const handlePosFeedback = () => setGood(good + 1)
-  const handleNeuFeedback = () => setNeutral(neutral + 1)
-  const handleBadFeedback = () => setBad(bad + 1)
+  const handlePosFeedback = () => {
+    setGood(good + 1)
+    setAverage(average + 1)
+    setFeedback(feedback + 1)
+  }
+
+  const handleNeuFeedback = () => {
+    setNeutral(neutral + 1)
+    setFeedback(feedback + 1)
+  }
+
+  const handleBadFeedback = () => {
+    setBad(bad + 1)
+    setAverage(average - 1)
+    setFeedback(feedback + 1)
+  }
 
   return (
     <div>
@@ -28,7 +49,7 @@ const App = () => {
       <Button onClick={handlePosFeedback} text='Good' />
       <Button onClick={handleNeuFeedback} text='Neutral' />
       <Button onClick={handleBadFeedback} text='Bad' />
-      <Results good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} average={average} feedback={feedback} />
     </div>
 
   )
