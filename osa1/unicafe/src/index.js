16ef58ecbdc181ebcd23b5de809d40267b397df7
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-08-29 19:34:58 +0300

    Unicafe step1 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Results = ({ good, neutral, bad }) =>{
+  return (
+    <div>
+      <p>Statistics</p>
+      <p>Good {good}<br />Neutral {neutral}<br />Bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handlePosFeedback = () => setGood(good + 1)
+  const handleNeuFeedback = () => setNeutral(neutral + 1)
+  const handleBadFeedback = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <p>Give feedback</p>
+      <Button onClick={handlePosFeedback} text='Good' />
+      <Button onClick={handleNeuFeedback} text='Neutral' />
+      <Button onClick={handleBadFeedback} text='Bad' />
+      <Results good={good} neutral={neutral} bad={bad} />
+    </div>
+
+  )
+}
+
+ReactDOM.render(<App />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
