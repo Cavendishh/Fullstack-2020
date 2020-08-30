c2fa756788614d802c75f8cb552d1b833e4d90be
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-08-30 16:23:02 +0300

    Unicafe step3 ja step4 toimii

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
+//Button creating component
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
+//Statistics creating component
 const Statistics = ({ good, neutral, bad, amount, average }) =>{
+  //If feedback has NOT been given, show this 
+  if (amount === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  //If feedback has been given, feedback is shown
   return (
     <div>
       <p>Statistics</p>
@@ -21,29 +33,34 @@ const Statistics = ({ good, neutral, bad, amount, average }) =>{
 }
 
 const App = () => {
+  //Set constants for feedback statistics
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [average, setAverage] = useState(0)
   const [amount, setAmount] = useState(0)
 
+  //Eventhandler for positive feedbacks
   const handlePosFeedback = () => {
     setGood(good + 1)
     setAverage(average + 1)
     setAmount(amount + 1)
   }
 
+  //Eventhandler for neutral feedbacks
   const handleNeuFeedback = () => {
     setNeutral(neutral + 1)
     setAmount(amount + 1)
   }
 
+  //Eventhandler for negative feedbacks
   const handleBadFeedback = () => {
     setBad(bad + 1)
     setAverage(average - 1)
     setAmount(amount + 1)
   }
 
+  //Creates the buttons and statistics with components
   return (
     <div>
       <p>Give feedback</p>
@@ -57,7 +74,6 @@ const App = () => {
         amount={amount}
         average={average} />
     </div>
-
   )
 }
 
