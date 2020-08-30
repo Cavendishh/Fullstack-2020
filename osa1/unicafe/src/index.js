435af7f74449b5f35ee03f544493ead7200afae8
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-08-30 16:13:47 +0300

    Unicafe step 2 toimii

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistics = ({ good, neutral, bad, average, feedback }) =>{
+const Statistics = ({ good, neutral, bad, amount, average }) =>{
   return (
     <div>
       <p>Statistics</p>
@@ -12,8 +12,9 @@ const Statistics = ({ good, neutral, bad, average, feedback }) =>{
         Good {good}<br />
         Neutral {neutral}<br />
         Bad {bad}<br />
-        Average {average}<br />
-        Feedback amount {feedback} 
+        Feedback amount {amount}<br />
+        Average {average / amount}<br />
+        Positive feedback {good / amount * 100 + ' %'}
       </p>
     </div>
   )
@@ -24,23 +25,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [average, setAverage] = useState(0)
-  const [feedback, setFeedback] = useState(0)
+  const [amount, setAmount] = useState(0)
 
   const handlePosFeedback = () => {
     setGood(good + 1)
     setAverage(average + 1)
-    setFeedback(feedback + 1)
+    setAmount(amount + 1)
   }
 
   const handleNeuFeedback = () => {
     setNeutral(neutral + 1)
-    setFeedback(feedback + 1)
+    setAmount(amount + 1)
   }
 
   const handleBadFeedback = () => {
     setBad(bad + 1)
     setAverage(average - 1)
-    setFeedback(feedback + 1)
+    setAmount(amount + 1)
   }
 
   return (
@@ -49,7 +50,12 @@ const App = () => {
       <Button onClick={handlePosFeedback} text='Good' />
       <Button onClick={handleNeuFeedback} text='Neutral' />
       <Button onClick={handleBadFeedback} text='Bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} average={average} feedback={feedback} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        amount={amount}
+        average={average} />
     </div>
 
   )
