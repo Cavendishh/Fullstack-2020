0f4cd50917c12da16d805947e69c2bd2b70f8dd0
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-08-31 16:14:21 +0300

    Most voted testaus

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = ({ anecdotes, votepoints }) => {
+  const [selected, setSelected] = useState(0)
+
+  const handleGenerateAnecdote = () => {
+    setSelected(Math.round(Math.random() * (anecdotes.length - 1)))
+  }
+
+  const handleAddVote = () => {
+    votepoints[selected] += 1
+  }
+
+  /*const mostVoted = () => {
+    const mostVoted = 0
+    const i = 0
+   
+    votepoints.forEach(value => {
+      if (value > 0) {
+        mostVoted = value
+      }
+    })
+  }*/
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <Button onClick={handleGenerateAnecdote} text='Anecdotes' />
+      <Button onClick={handleAddVote} text='Vote' />
+      <p>{anecdotes[selected]}</p>
+      <p>This anecdote has {votepoints[selected]} votes.</p>
+      <h3>Anecdote with the most votes</h3>
+    </div>
+  )
+}
+
+const votepoints = [0, 0, 0, 0, 0]
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90% of the development time.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as clearly as possible, you are, by definition, not smart enough to debug it'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} votepoints={votepoints} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
