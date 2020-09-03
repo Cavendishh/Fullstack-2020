4f846b247e052a41495edc09c39ceb46b4d44359
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-03 18:58:57 +0300

    Kurssitiedot step9 tehty, 2.4

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -14,35 +14,31 @@ const Course = ({ course }) => {
 const Header = ({ course }) => {
   return (
     <div>
-      <h1>{course['name']}</h1>
+      <h1>{course.name}</h1>
     </div>
   )
 }
 
 const Content = ({ course }) => {
   return (
-    <div>
-      <ul>
-        {course.parts.map(part =>
-        <li key={part.id}>{part.name} {part.exercises}</li>
-          )}
-      </ul>
-    </div>
+    course.parts.map(part => <Part key={part.id} value={part} />)
+  )
+}
+
+const Part = ({ value }) => {
+  return (
+    <p>
+      {value.name} {value.exercises}
+    </p>
   )
 }
 
 const Total = ({ course }) => {
-  let total = 0
-  let i = 0
-  
-  course.parts.forEach (() => {
-    total += course.parts[i].exercises
-    i++;
-  })
-
+  const total = course.parts.map(part => part.exercises).reduce((sum, currentValue) => sum + currentValue)
+    
   return(
     <div>
-      <p>Number of exercises {total}</p>
+      <p><b>Number of exercises {total}</b></p>
     </div>
   )
 }
@@ -51,22 +47,27 @@ const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-      id: 1
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-      id: 2
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-      id: 3
-    }
-  ]
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
   }
 
   return (
