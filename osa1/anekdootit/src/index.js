5dee21799e4fafd3da397c0febf1afa7310afc1e
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-03 17:50:39 +0300

    Anekdootit listaongelmat korjattu - koodi uusittu laajasti

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,41 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90% of the development time.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as clearly as possible, you are, by definition, not smart enough to debug it'
-]
-
-const votepoints = [0, 0, 0, 0, 0]
-
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const App = ({ anecdotes, votepoints }) => {
+const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0)
-  const [mostVotedValue, setMostVotedValue] = useState(0)
-  const [mostVotedAnecdote, setMostVotedAnecdote] = useState(anecdotes[0])
+  const [votes, setVotes] = useState(new Array(5).fill(0))
 
-  const handleGenerateAnecdote = () => {
-    setSelected(Math.round(Math.random() * (anecdotes.length - 1)))
-  }
+  const handleGenerateAnecdote = () => setSelected(Math.round(Math.random() * (anecdotes.length - 1)))
 
   const handleAddVote = () => {
-    votepoints[selected] += 1
+    const copyVotes = [...votes]
+    copyVotes[selected] += 1
+    setVotes(copyVotes)
   }
 
-  const generateMostVoted = () => {
-    let i = 0
+  const GenerateMostVoted = ({ votes, anecdotes }) => {
+    const maxVote = Math.max(...votes)
+    let maxIndex = votes.indexOf(maxVote)
 
-    votepoints.forEach(value => {
-      if (value > mostVotedValue) {
-       setMostVotedValue(value)
-       setMostVotedAnecdote(anecdotes[i])
-      }
-      i += 1
-    })
+    if (maxVote === 0) {
+      return (
+        <p>No anecdotes has been voted yet.</p>
+      )
+    }
+    return (
+      <p>"{anecdotes[maxIndex]}" has {votes[maxIndex]} votes.</p>
+    )
   }
   
   return (
@@ -44,15 +35,22 @@ const App = ({ anecdotes, votepoints }) => {
       <Button onClick={handleGenerateAnecdote} text='Anecdotes' />
       <Button onClick={handleAddVote} text='Vote' />
       <p>{anecdotes[selected]}</p>
-      <p>This anecdote has {votepoints[selected]} votes.</p>
+      <p>This anecdote has {votes[selected]} votes.</p>
       <h3>Anecdote with the most votes</h3>
-      {generateMostVoted()}
-      <p>"{mostVotedAnecdote}" has {mostVotedValue} votes.</p>
+      <GenerateMostVoted votes={votes} anecdotes={anecdotes} />
     </div>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90% of the development time.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as clearly as possible, you are, by definition, not smart enough to debug it'
+]
+
 ReactDOM.render(
-  <App anecdotes={anecdotes} votepoints={votepoints} />,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
