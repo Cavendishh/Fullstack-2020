ee4531fc8aa5bd742724709a9fd6ffd63ff63e39
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-05 17:50:47 +0300

    Tehtävä 2.4 tehty

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 const Course = ({ course }) => {
   return (
     <div>
+      <h1>Course list</h1>
       <Header course={course} />
       <Content course={course} />
       <Total course={course} />
@@ -14,7 +15,7 @@ const Course = ({ course }) => {
 const Header = ({ course }) => {
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </div>
   )
 }
@@ -44,36 +45,53 @@ const Total = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  
   return (
-    <div>
-      <Course course={course} />
-    </div>
+    courses.map((course) => <Course key={course.id} course={course} />)
   )
 }
 
