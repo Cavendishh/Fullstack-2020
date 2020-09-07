61b018e8ced8a366ce8ef3df0d5b6e2343a900ec
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-07 17:22:18 +0300

    2.7 puhelinluettelo tehty. Huom: Template string puuttuu

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -15,11 +15,15 @@ const App = () => {
 
     const noteObject = {
       name: newName,
-      id: persons.length + 1,
     }
 
-    setPersons(persons.concat(noteObject))
-    setNewName('')
+    if (persons.some((element) => element.name.toLowerCase() === newName.toLowerCase())) {
+      window.alert(newName + ' is already added to phonebook')
+      setNewName('')
+    } else {
+      setPersons(persons.concat(noteObject))
+      setNewName('')
+    }    
   }
 
   return (
@@ -32,7 +36,7 @@ const App = () => {
             onChange={handleNoteChange} />
         </div>
         <div>
-          <button type="submit">Add</button>
+          <button type='submit'>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
