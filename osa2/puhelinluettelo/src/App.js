5721c272e336b84c8d3c97e3eeeb15586b6ab8c3
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-12 18:08:55 +0300

    2.11: puhelinluettelo step6 tehty. 'Persons' lista siirretty databaseen

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,30 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import SearchPerson from './components/SearchPerson'
 import NewPerson from './components/NewPerson'
 import RenderPerson from './components/RenderPerson'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1231244'
-    },
-    { 
-      name: 'Ada Lovelace',
-      number: '39-44-5323523'
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345'
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122'
-    }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showPerson, setShowPerson] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  
+
+
 
   const handleNoteChange = (event) => setNewName(event.target.value)
 
