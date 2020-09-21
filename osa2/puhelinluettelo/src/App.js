9ded000b743a9a5e9ecb0af0782c868d0ea5ac7f
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-21 23:15:00 +0300

    Puhelinluettelo step 20 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 import SearchPerson from './components/SearchPerson'
 import NewPerson from './components/NewPerson'
 import RenderPerson from './components/RenderPerson'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('test')
+  const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showPerson, setShowPerson] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [messageStatus, setMessageStatus] = useState(null)
 
   const handleNoteChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
@@ -29,8 +32,24 @@ const App = () => {
       .remove(id)
       .then(() => {
         setPersons(persons.filter(n => n.id !== id));
-      })}
-    }    
+      })
+      .catch(() => {
+        setErrorMessage(`Information of ${newName} has already been removed from server`)
+        setMessageStatus('error')
+        setTimeout(() => {
+          setErrorMessage(null)
+          setMessageStatus(null)
+        }, 3000)
+        setPersons(persons.filter(n => n.id !== id));
+      })
+    setErrorMessage(`Removed ${name}`)
+    setMessageStatus('success')
+    setTimeout(() => {
+      setErrorMessage(null)
+      setMessageStatus(null)
+      }, 3000)
+    }
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -43,27 +62,50 @@ const App = () => {
     if (persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())) {
       if (window.confirm(newName + ' is already added to phonebook, replace the old number with a new one?')) {        
         const personFound = persons.find((person) => person.name === newName)
-        
+
         personService
           .update(personFound.id, { ...personFound, number: newNumber })
-          .then(response => setPersons(persons.map(person => (person.name === newName ? response : person))))
+          .then(response => {
+            setPersons(persons.map(person => (person.name === newName ? response : person)))
+          })
+          .catch(() => {
+            setErrorMessage(`Information of ${newName} has already been removed from server`)
+            setMessageStatus('error')
+              setTimeout(() => {
+                setErrorMessage(null)
+                setMessageStatus(null)
+              }, 3000)
+          })
+        setErrorMessage(`Changed ${newName}'s phone number`)
+        setMessageStatus('success')
+        setTimeout(() => {
+          setErrorMessage(null)
+          setMessageStatus(null)
+        }, 3000)        
+        setNewName('')
+        setNewNumber('')
       }
-      setNewName('')
-      setNewNumber('')
     } else {
       personService
         .create(personObject)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
-          setNewName('')
-          setNewNumber('')
         })
+      setErrorMessage(`Added ${newName}`)
+      setMessageStatus('success')
+      setTimeout(() => {
+        setErrorMessage(null)
+        setMessageStatus(null)
+      }, 3000)
+      setNewName('')
+      setNewNumber('')  
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} status={messageStatus} />
       <SearchPerson
         showPerson={showPerson}
         handleFilterChange={handleFilterChange}
@@ -83,4 +125,4 @@ const App = () => {
   )
 }
 
-export default App;
+export default App
