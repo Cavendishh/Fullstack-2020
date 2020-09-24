aa5d0a00538183b49554df87f8940ada6d7481c2
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-24 18:13:06 +0300

    Puhelinluettelo step 4-6 tehty

--- osa3/index.js
+++ osa3/index.js
@@ -1,8 +1,22 @@
-const { response } = require('express')
+//const { response } = require('express')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method', request.method)
+  console.log('Path', request.path)
+  console.log('Body', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(requestLogger)
 
 let persons = [
     {
@@ -24,6 +38,11 @@ let persons = [
       id: 4,
       name: "Mary Poppendieck",
       number: "39-23-6423122"
+    },
+    {
+      id: 5,
+      name: "test",
+      number: "111"
     }
   ]
 
@@ -38,6 +57,50 @@ app.get('/api/persons', (req, res) => {
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people.<br><br>${new Date()}</p>`)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!(body.name | body.number)) {
+    return res.status(400).json({
+      error: `Person's name or number is missing`
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+
+  const person = {
+    id: (Math.round(Math.random() * 9999 + 1)),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
