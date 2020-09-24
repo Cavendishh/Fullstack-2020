08c0cb9846eacd0883fdaf162fe3cce6dea10f46
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-24 18:41:37 +0300

    Morgan middleware vahtii loggausta

--- osa3/index.js
+++ osa3/index.js
@@ -1,22 +1,11 @@
-//const { response } = require('express')
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 
-const requestLogger = (request, response, next) => {
-  console.log('Method', request.method)
-  console.log('Path', request.path)
-  console.log('Body', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'Unknown endpoint' })
-}
-
-app.use(requestLogger)
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -100,9 +89,6 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-app.use(unknownEndpoint)
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
