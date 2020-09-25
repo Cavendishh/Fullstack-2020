7663928e9dcb9ff23a3837cc6788f23514cebd01
Cavendishh <66694518+Cavendishh@users.noreply.github.com>
2020-09-25 18:59:51 +0300

    Commit before hell

--- osa3/index.js
+++ osa3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 const port = process.env.PORT || 3001
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('object', function (req, res) {
